49d34b4c81660f885ba1ce2506029ea2df360033
gabuchannu <m.shimizu.0320@gmail.com>
2022-06-15 13:43:59 +0900

    H成分値を指定した範囲で絞って分布を出す

--- analysis_code/h_value_cut.py
+++ analysis_code/h_value_cut.py
@@ -25,7 +25,7 @@ def cut_data():
             count = 0
 
             for j in range(number): #全てのピクセルに対して処理を行う
-                if float(df.at[df.index[count], "H Value"]) >= 0 and float(df.at[df.index[count], "H Value"]) <= 19: #見たい部分だったら
+                if float(df.at[df.index[count], "H Value"]) >= 0 and float(df.at[df.index[count], "H Value"]) <= 16: #見たい部分だったら
                     count = count + 1
                 else: #見たい部分じゃなかったら
                     df = df.drop(df.index[[count]]) #該当データを全て消す
@@ -33,7 +33,7 @@ def cut_data():
             count = 0
 
             for k in range(number):
-                if float(df2.at[df2.index[count], "H Value"]) >= 20 and float(df2.at[df2.index[count], "H Value"]) <= 59: #見たい部分だったら
+                if float(df2.at[df2.index[count], "H Value"]) >= 0 and float(df2.at[df2.index[count], "H Value"]) <= 42.5: #見たい部分だったら
                     count = count + 1
                 else: #見たい部分じゃなかったら
                     df2 = df2.drop(df2.index[[count]]) #該当データを全て消す
@@ -41,15 +41,15 @@ def cut_data():
             count = 0
 
             for l in range(number):
-                if float(df3.at[df3.index[count], "H Value"]) >= 60: #見たい部分だったら
+                if float(df3.at[df3.index[count], "H Value"]) >= 212.5 and float(df3.at[df3.index[count], "H Value"]) <= 255: #見たい部分だったら
                     count = count + 1
                 else: #見たい部分じゃなかったら
                     df3 = df3.drop(df3.index[[count]]) #該当データを全て消す
                 
 
-            df_file = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_csv/shimizu/h_cut_data/h_delete_data_0_20_" + str(i+1) + ".csv"
-            df2_file = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_csv/shimizu/h_cut_data/h_delete_data_20_60_" + str(i+1) + ".csv"
-            df3_file = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_csv/shimizu/h_cut_data/h_delete_data_60_" + str(i+1) + ".csv"
+            df_file = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_csv/shimizu/h_cut_data/h_delete_data_0_16_" + str(i+1) + ".csv"
+            df2_file = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_csv/shimizu/h_cut_data/h_delete_data_0_42_" + str(i+1) + ".csv"
+            df3_file = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_csv/shimizu/h_cut_data/h_delete_data_212_255_" + str(i+1) + ".csv"
 
             df.to_csv(df_file, encoding="utf-8")
             df2.to_csv(df2_file, encoding="utf-8")
@@ -59,54 +59,129 @@ def cut_data():
 
 
 def make_graph(file_number):
-    analysis_data_1 = pd.read_csv("/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_csv/shimizu/h_cut_data/h_delete_data_0_20_" + file_number + ".csv")
-    analysis_data_2 = pd.read_csv("/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_csv/shimizu/h_cut_data/h_delete_data_20_60_" + file_number + ".csv")
-    analysis_data_3 = pd.read_csv("/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_csv/shimizu/h_cut_data/h_delete_data_60_" + file_number + ".csv")
+    analysis_data_1 = pd.read_csv("/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_csv/shimizu/h_cut_data/h_delete_data_0_16_" + file_number + ".csv")
+    analysis_data_2 = pd.read_csv("/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_csv/shimizu/h_cut_data/h_delete_data_0_42_" + file_number + ".csv")
+    analysis_data_3 = pd.read_csv("/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_csv/shimizu/h_cut_data/h_delete_data_212_255_" + file_number + ".csv")
     
     fig_1 = pyplot.figure()
     ax_1 = fig_1.add_subplot(1, 1, 1)
-    ax_1.scatter("S Value", "V Value", data=analysis_data_1)
+    ax_1.scatter("V Value", "S Value", data=analysis_data_1)
 
-    pyplot.title("S and V Value(H Value 0~19)") #グラフタイトル
-    pyplot.xlabel("S Value") #x軸
-    pyplot.ylabel("V Value") #y軸
+    pyplot.title("S and V Value(H Value 0~16)") #グラフタイトル
+    pyplot.xlabel("V Value") #x軸
+    pyplot.ylabel("S Value") #y軸
     pyplot.xlim(0, 255)
     pyplot.ylim(0, 255)
 
     fig_2 = pyplot.figure()
     ax_2 = fig_2.add_subplot(1, 1, 1)
-    ax_2.scatter("S Value", "V Value", data=analysis_data_2)
+    ax_2.scatter("V Value", "S Value", data=analysis_data_2)
 
-    pyplot.title("S and V Value(H Value 20~59)") #グラフタイトル
-    pyplot.xlabel("S Value") #x軸
-    pyplot.ylabel("V Value") #y軸
+    pyplot.title("S and V Value(H Value 0~42)") #グラフタイトル
+    pyplot.xlabel("V Value") #x軸
+    pyplot.ylabel("S Value") #y軸
     pyplot.xlim(0, 255)
     pyplot.ylim(0, 255)
 
     fig_3 = pyplot.figure()
     ax_3 = fig_3.add_subplot(1, 1, 1)
-    ax_3.scatter("S Value", "V Value", data=analysis_data_3)
+    ax_3.scatter("V Value", "S Value", data=analysis_data_3)
 
-    pyplot.title("S and V Value(H Value 60~)") #グラフタイトル
-    pyplot.xlabel("S Value") #x軸
+    pyplot.title("S and V Value(H Value 212~255)") #グラフタイトル
+    pyplot.xlabel("V Value") #x軸
+    pyplot.ylabel("S Value") #y軸
+    pyplot.xlim(0, 255)
+    pyplot.ylim(0, 255)
+
+    fig_4 = pyplot.figure()
+    ax_4 = fig_4.add_subplot(1, 1, 1)
+    ax_4.scatter("H Value", "S Value", data=analysis_data_1)
+
+    pyplot.title("S and H Value(H Value 0~16)") #グラフタイトル
+    pyplot.xlabel("H Value") #x軸
+    pyplot.ylabel("S Value") #y軸
+    pyplot.xlim(0, 255)
+    pyplot.ylim(0, 255)
+
+    fig_5 = pyplot.figure()
+    ax_5 = fig_5.add_subplot(1, 1, 1)
+    ax_5.scatter("H Value", "S Value", data=analysis_data_2)
+
+    pyplot.title("S and H Value(H Value 0~42)") #グラフタイトル
+    pyplot.xlabel("H Value") #x軸
+    pyplot.ylabel("S Value") #y軸
+    pyplot.xlim(0, 255)
+    pyplot.ylim(0, 255)
+
+    fig_6 = pyplot.figure()
+    ax_6 = fig_6.add_subplot(1, 1, 1)
+    ax_6.scatter("H Value", "S Value", data=analysis_data_3)
+
+    pyplot.title("S and H Value(H Value 212~255)") #グラフタイトル
+    pyplot.xlabel("H Value") #x軸
+    pyplot.ylabel("S Value") #y軸
+    pyplot.xlim(0, 255)
+    pyplot.ylim(0, 255)
+
+    fig_7 = pyplot.figure()
+    ax_7 = fig_7.add_subplot(1, 1, 1)
+    ax_7.scatter("H Value", "V Value", data=analysis_data_1)
+
+    pyplot.title("H and V Value(H Value 0~16)") #グラフタイトル
+    pyplot.xlabel("H Value") #x軸
     pyplot.ylabel("V Value") #y軸
     pyplot.xlim(0, 255)
     pyplot.ylim(0, 255)
 
-    file_name_1 = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_graph/shimizu/h_cut_data/0_19_" + str(file_number) + ".png"
-    file_name_2 = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_graph/shimizu/h_cut_data/20_59_" + str(file_number) + ".png"
-    file_name_3 = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_graph/shimizu/h_cut_data/60_" + str(file_number) + ".png"
+    fig_8 = pyplot.figure()
+    ax_8 = fig_8.add_subplot(1, 1, 1)
+    ax_8.scatter("H Value", "V Value", data=analysis_data_2)
+
+    pyplot.title("H and V Value(H Value 0~42)") #グラフタイトル
+    pyplot.xlabel("H Value") #x軸
+    pyplot.ylabel("V Value") #y軸
+    pyplot.xlim(0, 255)
+    pyplot.ylim(0, 255)
+
+    fig_9 = pyplot.figure()
+    ax_9 = fig_9.add_subplot(1, 1, 1)
+    ax_9.scatter("H Value", "V Value", data=analysis_data_3)
+
+    pyplot.title("H and V Value(H Value 212~255)") #グラフタイトル
+    pyplot.xlabel("H Value") #x軸
+    pyplot.ylabel("V Value") #y軸
+    pyplot.xlim(0, 255)
+    pyplot.ylim(0, 255)
+
+    file_name_1 = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_graph/shimizu/h_cut_data/sv_0_16_" + str(file_number) + ".png"
+    file_name_2 = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_graph/shimizu/h_cut_data/sv_0_42_" + str(file_number) + ".png"
+    file_name_3 = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_graph/shimizu/h_cut_data/sv_212_255" + str(file_number) + ".png"
+    file_name_4 = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_graph/shimizu/h_cut_data/hs_0_16_" + str(file_number) + ".png"
+    file_name_5 = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_graph/shimizu/h_cut_data/hs_0_42_" + str(file_number) + ".png"
+    file_name_6 = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_graph/shimizu/h_cut_data/hs_212_255" + str(file_number) + ".png"
+    file_name_7 = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_graph/shimizu/h_cut_data/hv_0_16_" + str(file_number) + ".png"
+    file_name_8 = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_graph/shimizu/h_cut_data/hv_0_42_" + str(file_number) + ".png"
+    file_name_9 = "/Users/shimizu_italab/Desktop/Study/HSV_Experiment/result_graph/shimizu/h_cut_data/hv_212_255" + str(file_number) + ".png"
 
     pyplot.close(fig_1)
     pyplot.close(fig_2)
     pyplot.close(fig_3)
+    pyplot.close(fig_4)
+    pyplot.close(fig_5)
+    pyplot.close(fig_6)
+    pyplot.close(fig_7)
+    pyplot.close(fig_8)
+    pyplot.close(fig_9)
 
     fig_1.savefig(file_name_1)
     fig_2.savefig(file_name_2)
     fig_3.savefig(file_name_3)
-
-
-
+    fig_4.savefig(file_name_4)
+    fig_5.savefig(file_name_5)
+    fig_6.savefig(file_name_6)
+    fig_7.savefig(file_name_7)
+    fig_8.savefig(file_name_8)
+    fig_9.savefig(file_name_9)
 
 
 #-------------
